7b5e4ff4cbdbb87bb1fbb31fe438f53781405b21
Roberth <roberth.fallas.urena@est.una.ac.cr>
2021-10-09 23:29:00 -0600

    Interfaz lista para iniciar a trabajar

--- src/proyecto2/controller/BaseController.java
+++ src/proyecto2/controller/BaseController.java
@@ -5,13 +5,18 @@
  */
 package proyecto2.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+import proyecto2.Proyecto2;
 
 /**
+ * FXML Controller class
  *
  * @author rober
  */
@@ -23,10 +28,27 @@ public class BaseController implements Initializable {
     private Tab tab_puestos;
     @FXML
     private Tab tab_planillas;
-    
+
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        tab_empleados.setContent(this.getTabContent("EmpleadosView"));
+        tab_puestos.setContent(this.getTabContent("PuestosView"));
+        tab_planillas.setContent(this.getTabContent("PlanillasView"));
+    }
+
+    private AnchorPane getTabContent(String name){
+        try{
+            AnchorPane root = FXMLLoader.load(Proyecto2.class.getResource("view/"+name+".fxml"));
+            return root;
+        }catch(IOException IO){
+            return null;
+        }
+    }
     
 }
